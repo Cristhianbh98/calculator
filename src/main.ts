db441898359dc20b7feb1d6e1742a059f45c6826
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2022-02-06 17:45:49 -0500

    feat: dark mode

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import './main.scss'
+
+// Global var
+const html = document.querySelector('html')
+
+// Change the theme
+
+const toggleThemeButton = document.getElementById('toggle-theme')
+
+toggleThemeButton?.addEventListener('click', () => {
+  const currentTheme = toggleThemeButton?.getAttribute('data-theme')
+
+  toggleTheme(currentTheme ? currentTheme : '')
+})
+
+function toggleTheme(currentTheme: string) {  
+  if (currentTheme === 'light') {
+    html?.classList.add('dark-mode')
+    toggleThemeButton?.setAttribute('data-theme', 'dark')
+  } else {
+    html?.classList.remove('dark-mode')
+    toggleThemeButton?.setAttribute('data-theme', 'light')
+  }
+}
