28fc5d818e9206d1515041096e893d368c5adb29
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2022-02-17 17:35:47 -0500

    add: tooltip to options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import './main.scss'
 // import modules
 import Calculator from './modules/Calculator'
 import ToggleTheme from './modules/ToggleTheme'
+import Popper from './modules/Popper'
 
 // instance class
 new Calculator()
 new ToggleTheme('#toggle-theme-btn')
+
+// popper
+new Popper('#toggle-theme-btn', '#toggle-theme-btn-tooltip')
+new Popper('#toggle-history-btn', '#toggle-history-btn-tooltip')
+new Popper('#delete-history-btn', '#delete-history-btn-tooltip', 'top-end')
---
+++ src/modules/Popper.ts
@@ -0,0 +1,49 @@
+import { createPopper, Instance, Placement } from '@popperjs/core'
+
+class Popper {
+  button: HTMLElement
+  tooltip: HTMLElement
+  popperInstance: Instance
+
+  constructor(buttonSelector: string, tooltipSelector: string, placement: Placement = 'top') {
+    this.button = document.querySelector(buttonSelector)!
+    this.tooltip = document.querySelector(tooltipSelector)!
+
+    this.popperInstance = createPopper(this.button, this.tooltip, {      
+      placement: placement,
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8]
+          }
+        }
+      ]      
+    })
+    this.events()
+  }
+
+  events() {
+    const showEvents = ['mouseenter', 'focus']
+    const hideEvents = ['mouseleave', 'blur']
+
+    showEvents.forEach((event) => {
+      this.button.addEventListener(event, () => this.show());
+    })
+    
+    hideEvents.forEach((event) => {
+      this.button.addEventListener(event, () => this.hide());
+    })
+  }
+
+  show() {
+    this.tooltip.setAttribute('data-show', '')
+    this.popperInstance.update()
+  }
+
+  hide() {
+    this.tooltip.removeAttribute('data-show')
+  }
+}
+
+export default Popper
